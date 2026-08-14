5f6203dc5f8d81748c3faa5fab49235e106de447
aristov983 <earis4499@gmail.com>
2026-08-14 01:53:01 -0300

    concepto de datos ahora si

--- T1_RegistroTemperaturas/clientes/temperatureRecorderUTP.js
+++ T1_RegistroTemperaturas/clientes/temperatureRecorderUTP.js
@@ -3,6 +3,26 @@ import {Buffer} from 'node:buffer';
 
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-const getRandomTemperature = ()=>{return Math.floor(Math.random()*60)}
+const datos = {
+    temperatura: 21,
+    hora:0,
+    minuto:0,
+    segundo:0,
+    dia:0,
+    mes:0,
+    anio:0,
+}
 
-console.log(getRandomTemperature())
+function actualizarDatos(){
+    const tiempo = new Date(); //Nota: Esto va adentro de la función porque se actualiza cuando se invoca.
+    datos.temperatura+=Math.random() * 0.2-0.1;
+    datos.hora = tiempo.getHours();
+    datos.minuto = tiempo.getMinutes();
+    datos.segundo = tiempo.getSeconds();
+    datos.dia = tiempo.getDate(); //getDay() devuelve el día de la semana, es getDate()
+    datos.mes = tiempo.getMonth()+1;
+    datos.anio = tiempo.getFullYear();
+}
+
+actualizarDatos();
+console.log(datos);
